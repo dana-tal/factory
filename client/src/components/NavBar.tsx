74f35a92410cb038d4dfd38d0dd5c89b8c59383f
Dana Tal <dana-tal@012.net.il>
2026-05-21 09:31:25 +0300

    adding Typescript to NavBar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -4,11 +4,29 @@ import { Box } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 import "./NavBar.css";
 
+// The object may have a string link prop and in that case callback prop does not exist
+// or the object may have a callback prop, in that case link prop does not exit 
+type NavLinkItem =
+  | {
+      name: string;
+      link: string;
+      callback?: never;
+    }
+  | {
+      name: string;
+      callback: () => void;
+      link?: never;
+    };
 
-function NavBar({ links }) {
+
+type NavBarProps = {
+  links: NavLinkItem[];
+};
+
+function NavBar({links}:NavBarProps ) {
    return (
     <Box className="navbar-container">
-      <Stack className="navbar-stack" direction="row"   sx={{ flexWrap: 'wrap' ,columnGap: 1, rowGap: { xs: 1, sm: 2 } }} gap={2}>
+      <Stack className="navbar-stack" direction="row"   sx={{ flexWrap: 'wrap' ,gap:2, columnGap: 1, rowGap: { xs: 1, sm: 2 } }} >
         { links.map( link =>{ 
              
              if ( link.link)
